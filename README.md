737e2778bd54c6826c3e57906cdc6d6be3a986ec
jerry george <jerrygithub360@gmail.com>
2023-03-02 16:12:21 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,21 @@
+#  TranslateApi
+
+ **This uses the openAPI gpt-3 api.**
+
+**Usage : make an api call to the endpoint "/translate" and follow the format below:**
+
+**what is needed from you are:**
++ the data to be translated and 
++ the language.
+
+ 
+    async function translate(){
+    let data= data
+    let language=language
+    let response
+    let translated
+    response=await fetch("/translate",{method:"POST",headers: { "Content-type": "application/json" },
+     body: JSON.stringify({"data":data,"language":language})})
+    translated=await response.text()
+    console.log(translated)
+}
---
+++ app.py
@@ -0,0 +1,27 @@
+import os
+import openai
+from flask import Flask, request, render_template
+
+app = Flask(__name__)
+openai.api_key = os.getenv("OPENAI_API_KEY")
+
+
+@app.post("/translate")
+def translate():
+    front_end_res = request.get_json()
+    language = front_end_res["language"]
+    what_to_translate = front_end_res["data"]
+    response = openai.Completion.create(
+        model="text-davinci-003",
+        prompt=f"Translate this into {language}\n{what_to_translate}",
+        temperature=0.9,
+        max_tokens=150,
+        top_p=1,
+        frequency_penalty=0,
+        presence_penalty=0.6,
+    )
+    return response.choices[0].text
+
+
+if __name__ == '__main__':
+    app.run()
